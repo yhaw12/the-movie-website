488201b8611b6666d13ac2e8c61a4f4e9a246cd3
yhaw12 <yooxing11@gmail.com>
2023-02-25 23:44:48 +0000

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,22 @@ closeBtn.addEventListener('click', ()=>{
     closeBtn.style.display = 'none'
     menuBar.style.display = 'block';
 })
+
+// media query javascrip
+let searchImage = document.querySelector('.search-image');
+let searchBar = document.querySelector('.searchbar')
+let input = document.querySelector('.input');
+
+searchImage.addEventListener('click', ()=>{
+    searchBar.classList.toggle = 'show';
+     console.log('i am clicked')
+    input.style.display = 'block';
+})
+
+// sidebar copyright
+
+let date = document.querySelector('.date');
+
+let footerDate = new Date().getFullYear();
+
+date.textContent = footerDate;
