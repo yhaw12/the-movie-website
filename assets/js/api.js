08fda22ab37c74de78210d7f5add7ed09d3b1052
yhaw12 <yooxing11@gmail.com>
2023-09-05 22:05:50 +0000

    naa

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,15 @@
+// Select the elements within your HTML structure
+const movieTitle = document.querySelector('.movie-text h1');
+const yearElement = document.querySelector('.movie-text .year');
+const ratingElement = document.querySelector('.movie-text .rating');
+const genresElement = document.querySelector('.movie-text span');
+const descriptionElement = document.querySelector('.movie-text p');
+const buttonElement = document.querySelector('.movie-text .btn');
+const smImages = document.querySelectorAll('.sm-image img');
+
+
+
+// MOVIE SECTION API
 const tpTrending = document.querySelector('.popular'); 
 const mdTrending = document.querySelector('.trendy'); 
 const bdTrending = document.querySelector('.upcoming-movies'); 
@@ -10,6 +22,33 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Zjg4ZWZkNjc3ZTgyMWU1ZTYzMDc1YmY3OWU4ZWI1NCIsInN1YiI6IjYzZDg2MjY3MTJiMTBlMDA5M2U4ZmMwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MTD7ZVryJwr3RlLfQV-DW8gLrrig1GOgsVwbvkd092M'
     }
 };
+
+
+    // TOP BANNER
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(data=>{
+          let movieDatas = data.results;
+
+          function updateMovieDetails(movieData) {
+            movieTitle.textContent = movieData.title;
+            yearElement.textContent = movieData.release_date.slice(0, 4);
+            ratingElement.textContent = movieData.vote_average;
+            genresElement.textContent = movieData.genres.map(genre => genre.name).join(', ');
+            descriptionElement.textContent = movieData.overview;
+          }
+
+
+          function handleSmImageClick(e){
+            const clickedImageIndex = Array.from(smImages).indexOf(e.target);
+            if(clickedImageIndex !== -1){
+              const selectedMovie = movieDatas[clickedImageIndex];
+              updateMovieDetails(selectedMovie); 
+            }
+            
+          }
+        })
+        .catch(err => console.error(err));
       
 
       // TOP RATED MOVIES
@@ -84,7 +123,6 @@ const options = {
         fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
          .then(response => response.json())
             .then(data=>{
-
                let upcomingMovie ='';
 
                data.results.forEach((up, index)=>{
@@ -115,7 +153,4 @@ const options = {
             .catch(err => console.error(err));
 
 
-            fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-            .then(response => response.json())
-            .then(response => console.log(response))
-            .catch(err => console.error(err));
+            
