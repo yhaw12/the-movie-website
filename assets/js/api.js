5c9c6c86569fd61b944692a5cb38500cc0d12931
yhaw12 <yooxing11@gmail.com>
2023-09-07 22:45:56 +0000

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,13 +1,4 @@
 // Select the elements within your HTML structure
-const movieTitle = document.querySelector('.movie-text h1');
-const yearElement = document.querySelector('.movie-text .year');
-const ratingElement = document.querySelector('.movie-text .rating');
-const genresElement = document.querySelector('.movie-text span');
-const descriptionElement = document.querySelector('.movie-text p');
-const buttonElement = document.querySelector('.movie-text .btn');
-const smImages = document.querySelectorAll('.sm-image img');
-
-
 
 // MOVIE SECTION API
 const tpTrending = document.querySelector('.popular'); 
@@ -25,67 +16,23 @@ const options = {
 
 
     // TOP BANNER
-    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-        .then(response => response.json())
-        .then(data=>{
-          let movieDatas = data.results;
+   const smImage = document.querySelector('.sm-image');
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(data => {
+        let movieData = '';
 
-          function updateMovieDetails(movieData) {
-            movieTitle.textContent = movieData.title;
-            yearElement.textContent = movieData.release_date.slice(0, 4);
-            ratingElement.textContent = movieData.vote_average;
-            genresElement.textContent = movieData.genres.map(genre => genre.name).join(', ');
-            descriptionElement.textContent = movieData.overview;
-          }
-
-
-          function handleSmImageClick(e){
-            const clickedImageIndex = Array.from(smImages).indexOf(e.target);
-            if(clickedImageIndex !== -1){
-              const selectedMovie = movieDatas[clickedImageIndex];
-              updateMovieDetails(selectedMovie); 
-            }
-            
-          }
+        data.results.forEach((movie)=>{
+          movieData+=`<img class="ac-img" src="http://image.tmdb.org/t/p/w500/${movie.poster_path}"/>`
         })
-        .catch(err => console.error(err));
-      
+        smImage.innerHTML = movieData
+    })
+    .catch(err => console.error(err));
 
-      // TOP RATED MOVIES
-      fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-        .then(response => response.json())
-        .then(data =>{
-          // console.log(data)
-            let movieData = '';
-
-            data.results.forEach((value, index)=>{
-              movieData+= `
-                   <div class="card key=${index}  ">
-                      <div class="card-box">
-                        <div class="image">
-                            <img src="http://image.tmdb.org/t/p/w500/${value.poster_path}">
-                        </div>
-                        <h3>${value.title}</h3>
-                        <div class="ratings">
-                          <div class="left">
-                            <img src="assets/images/star.png">
-                            <span>${value.vote_average}</span>
-                        </div>
-
-                        <div class="right">
-                            <span>${value.vote_count}</span>
-                        </div> 
-                    </div>
-                     
-                  </div>`
-            })
-            tpTrending.innerHTML = movieData; 
-        })
-        .catch(err => console.error(err));
 
 
         // WEEKLY TRENDING
-        fetch('https://api.themoviedb.org/3/trending/movie/week?language=en-US&page=1', options)
+        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
           .then(response => response.json())
           .then(data =>{
             let trendyMovie ='';
