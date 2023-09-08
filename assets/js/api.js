2c2a76bc0f3fb423045618f668b23f0175fe7ac1
yhaw12 <yooxing11@gmail.com>
2023-09-08 18:32:55 +0000

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -7,56 +7,81 @@ const bdTrending = document.querySelector('.upcoming-movies');
 
 
 const options = {
-    method: 'GET',
-      headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Zjg4ZWZkNjc3ZTgyMWU1ZTYzMDc1YmY3OWU4ZWI1NCIsInN1YiI6IjYzZDg2MjY3MTJiMTBlMDA5M2U4ZmMwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MTD7ZVryJwr3RlLfQV-DW8gLrrig1GOgsVwbvkd092M'
-    }
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Zjg4ZWZkNjc3ZTgyMWU1ZTYzMDc1YmY3OWU4ZWI1NCIsInN1YiI6IjYzZDg2MjY3MTJiMTBlMDA5M2U4ZmMwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MTD7ZVryJwr3RlLfQV-DW8gLrrig1GOgsVwbvkd092M'
+  }
 };
 
 
     // TOP BANNER
-   const smImage = document.querySelector('.sm-image');
-fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
-    .then(response => response.json())
-    .then(data => {
-        let movieData = '';
+   function fetchMovieData(){
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response=> response.json())
+    .then(data =>{
+      let currentIndex = 0;
+      const smallImages = data.results.map(movie=> movie.poster_path)
 
-        data.results.forEach((movie)=>{
-          movieData+=`<img class="ac-img" src="http://image.tmdb.org/t/p/w500/${movie.poster_path}"/>`
-        })
-        smImage.innerHTML = movieData
+
+       // Function to update content with the next movie
+       function updateContent(index) {
+        const movie = data.results[index];
+        document.getElementById('bgImage').src = `http://image.tmdb.org/t/p/w500/${movie.backdrop_path}`;
+        document.getElementById('movieTitle').textContent = movie.title;
+        document.getElementById('movieYear').textContent = movie.release_date.split('-')[0];
+        document.getElementById('movieRating').textContent = movie.vote_average;
+        // document.getElementById('movieGenre').textContent = movie.genres.map(genre => genre.name).join(', ');
+        document.getElementById('movieDescription').textContent = movie.overview;
+      }
+
+       
+
+       updateContent(currentIndex);
+       
+
+        // Set interval to change small images and update content
+       setInterval(()=>{
+        currentIndex = (currentIndex + 1) ;
+        document.getElementById('smallImages').innerHTML = `
+            <img src="http://image.tmdb.org/t/p/w185/${smallImages[currentIndex]}">
+        `;
+        updateContent(currentIndex);
+       }, 2500)
     })
-    .catch(err => console.error(err));
+    .catch(err => console.error('Error fetching data:', err));
+   }
+
+    fetchMovieData();
 
 
 
         // WEEKLY TRENDING
-        fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
           .then(response => response.json())
           .then(data =>{
+            console.log(data)
             let trendyMovie ='';
 
           data.results.forEach((trendy, index)=>{
             trendyMovie+= `
-                  <div class="card key=${index}  ">
-                    <div class="card-box">
-                      <div class="image">
-                          <img src="http://image.tmdb.org/t/p/w500/${trendy.poster_path}">
-                      </div>
-                      <h3>${trendy.title}</h3>
-                      <div class="ratings">
-                        <div class="left">
-                          <img src="assets/images/star.png">
+              <div class="card key=${index}  ">
+                <div class="card-box">
+                <div class="image">
+                    <img src="http://image.tmdb.org/t/p/w500/${trendy.poster_path}">
+                </div>
+                <h3>${trendy.title}</h3>
+                  <div class="ratings">
+                    <div class="left">
+                      <img src="assets/images/star.png">
                           <span>${trendy.vote_average}</span>
                       </div>
 
                       <div class="right">
                           <span>${trendy.vote_count}</span>
                       </div> 
-                  </div>
-                    
-                </div>`
+                    </div>  
+                  </div>`
           })
 
           mdTrending.innerHTML = trendyMovie; 
@@ -98,6 +123,3 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', op
 
             })
             .catch(err => console.error(err));
-
-
-            
